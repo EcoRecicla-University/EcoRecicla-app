4e4442085ade2a5233158f2e11219d43b17b39fd
EricVB <eric.viniciusbasso@gmail.com>
2025-03-11 15:54:48 -0300

    feat: adicionando redirecionamento.

--- ecorecicla-app/src/app/app.routing.ts
+++ ecorecicla-app/src/app/app.routing.ts
@@ -2,8 +2,16 @@ import { ModuleWithProviders } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
 
 export const APP_ROUTES: Routes = [
-    { path: 'login', component: LoginComponent }
+    { 
+        path: 'login', component: LoginComponent 
+    },
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    }
 ]
-// export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES)
