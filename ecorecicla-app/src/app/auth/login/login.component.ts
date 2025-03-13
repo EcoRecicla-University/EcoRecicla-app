5d51e423717f3043f1cdb00e2d3a511f0e1cf480
EricVB <eric.viniciusbasso@gmail.com>
2025-03-13 15:59:08 -0300

    feat: Instalacao da biblioteca @angular/material

--- ecorecicla-app/src/app/auth/login/login.component.ts
+++ ecorecicla-app/src/app/auth/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import {
+    Validators,
+    FormsModule,
+    ReactiveFormsModule,
+  } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
 
 @Component ({
     selector: 'app-login',
     templateUrl: 'login.component.html',
-    imports: [RouterLink]
+    imports: [RouterLink, MatInputModule, FormsModule, ReactiveFormsModule]
 })
 
 export class LoginComponent {
+
+    
 
 }
