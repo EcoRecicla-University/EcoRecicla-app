eb2fbc4f6b02f4dfd246cb42a320662068ed51ec
Eric-VB <eric.viniciusbasso@gmail.com>
2025-03-13 21:04:58 -0300

    style: Criando view de login responsiva.

--- ecorecicla-app/src/app/auth/login/login.component.ts
+++ ecorecicla-app/src/app/auth/login/login.component.ts
@@ -1,17 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import {
-    Validators,
-    FormsModule,
-    ReactiveFormsModule,
-  } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 
 @Component ({
     selector: 'app-login',
     templateUrl: 'login.component.html',
-    imports: [RouterLink, MatInputModule, FormsModule, ReactiveFormsModule]
+    imports: [RouterLink, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule]
 })
 
 export class LoginComponent {
