9445adaa3936e5ef1037193b6a086a0ae508c5e3
Eric-VB <eric.viniciusbasso@gmail.com>
2025-03-26 17:49:36 -0300

    style: Criando tela de rota.

--- ecorecicla-app/src/app/pages/rota/rota.component.ts
+++ ecorecicla-app/src/app/pages/rota/rota.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { provideNativeDateAdapter } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 @Component ({
-    selector: 'app-rota',
-    templateUrl: 'rota.component.html',
-    imports: []
+    selector: 'app--pages-rota',
+    templateUrl: './rota.component.html',
+    providers: [provideNativeDateAdapter()],
+    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule]
 })
 export class RotaComponent{
 
