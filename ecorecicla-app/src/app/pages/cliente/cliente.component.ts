2c39282ede3e7c9976b2da65ab78d3d12ccf4b93
Eric-VB <eric.viniciusbasso@gmail.com>
2025-03-26 16:22:16 -0300

    style: Criando tela de clientes.

--- ecorecicla-app/src/app/pages/cliente/cliente.component.ts
+++ ecorecicla-app/src/app/pages/cliente/cliente.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component ({
     selector: 'app-pages-cliente',
     templateUrl: './cliente.component.html',
-    imports: []
+    imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule, MatCheckboxModule, MatRadioModule]
 })
 export class PagesClienteComponent {
     
