a657a2fbbfa88941c2b80705a42d64ddf6569d30
Eric-VB <eric.viniciusbasso@gmail.com>
2025-05-03 21:28:46 -0300

    feat: Adicionando formGoup cadastro de funcionario.

--- ecorecicla-app/src/app/pages/funcionario/funcionarios/cadastro/cadastro.component.ts
+++ ecorecicla-app/src/app/pages/funcionario/funcionarios/cadastro/cadastro.component.ts
@@ -6,13 +6,38 @@ import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from "@angular/material/core";
+import { FuncionarioService } from "../../../../core/services/funcionarios/funcionario.service";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component ({
     selector: 'app-pages-cadastro',
     templateUrl: './cadastro.component.html',
     providers: [provideNativeDateAdapter()],
-    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule]
+    imports: [
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatSelectModule,
+        ReactiveFormsModule,
+    ]
 })
 export class PagesFuncionariosFuncionariosCadastroComponent {
+
+    public form = new FormGroup({
+        nome: new FormControl('', Validators.required),
+        dataNascimento: new FormControl('', Validators.required),
+        telefone: new FormControl('', Validators.required),
+        cpf: new FormControl('', Validators.required),
+        rg: new FormControl('', Validators.required),
+        dataContratacao: new FormControl(null, Validators.required),
+        estadoCivil: new FormControl(null, Validators.required)
+    })
+
     readonly startDate = new Date(1990, 0, 1);
+
+    constructor(private _service: FuncionarioService) { }
+
+    salvar(){
+
+    }
 }
