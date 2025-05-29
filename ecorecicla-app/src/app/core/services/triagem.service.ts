8176e70ef7e231e90e175125885a04d964b2a63e
Eric-VB <eric.viniciusbasso@gmail.com>
2025-05-28 22:20:35 -0300

    feat: Listagem de centros de triagem.

--- ecorecicla-app/src/app/core/services/triagem.service.ts
+++ ecorecicla-app/src/app/core/services/triagem.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { CadastroTriagemModel } from "../models/private/triagem/triagemCadastro.model";
+import { CadastroTriagemModel } from "../models/private/triagem/cadastroTriagem.model";
 import { Observable } from "rxjs";
+import { ListagemTriagemModel } from "../models/private/triagem/listagemTriagem.model";
 
 @Injectable({
     providedIn: 'root'
@@ -11,9 +12,14 @@ export class TriagemService {
     private apiUrl = 'http://localhost:8080/api/triagem';
     
     constructor(private http: HttpClient) { }
+    
+    getCentrosTriagem(){
+        return this.http.get<ListagemTriagemModel[]>(this.apiUrl, {})
+    }
 
     criarNovoCentroTriagem(dados: CadastroTriagemModel): Observable<CadastroTriagemModel> {
         const apiUrl = `${this.apiUrl}`;
         return this.http.post<CadastroTriagemModel>(apiUrl, dados);
     }
+
 }
