c8631cdc87197d27d8f6a83a51c9d4801382fa86
Eric-VB <eric.viniciusbasso@gmail.com>
2025-06-22 22:12:34 -0300

    fix: reexibindo status da coleta no detalhe.

--- ecorecicla-app/src/app/core/models/private/coleta/editarColeta.model.ts
+++ ecorecicla-app/src/app/core/models/private/coleta/editarColeta.model.ts
@@ -1,7 +1,10 @@
+import { StatusColetaEnum } from "./listaColeta.model";
+
 export interface EditarColetaModel{
     Nome: string;
     ID_Coleta: string;
     Data_Coleta: string;
     ID_Cliente: string;
     Quantidade: string;
+    Status_Coleta?: StatusColetaEnum;
 }
