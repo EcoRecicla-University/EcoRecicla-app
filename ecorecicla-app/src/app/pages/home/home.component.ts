2879fc620d6c69ee96e4e4e494a4aba26e0f9fae
Eric-VB <eric.viniciusbasso@gmail.com>
2025-06-23 15:00:44 -0300

    style: atualizando tela de home.

--- ecorecicla-app/src/app/pages/home/home.component.ts
+++ ecorecicla-app/src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component ({
     selector: 'app-home',
     templateUrl: 'home.component.html',
-    imports: [MatIconModule]
+    imports: [
+        MatIconModule,
+        RouterLink,
+        MatButtonModule
+    ]
 })
 export class HomeComponent {
     constructor(){}
