aac62322f5f5b5477d10f8be7e75eb4846618566
Eric-VB <eric.viniciusbasso@gmail.com>
2025-06-25 01:16:47 -0300

    feat: modificando tela de login

--- ecorecicla-app/src/app/auth/login/login.component.ts
+++ ecorecicla-app/src/app/auth/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent {
 
     formularioLogin = new FormGroup({
         email: new FormControl('eric.viniciusbasso@gmail.com', [Validators.required, Validators.email]),
-        password: new FormControl('1TTYVJB685', [Validators.required])
+        password: new FormControl('4IMcTY2okv', [Validators.required])
     });
 
     loginSuccess:boolean = null
