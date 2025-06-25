24fb67082c89e87290f461a2116a4bb1623a4978
Eric-VB <eric.viniciusbasso@gmail.com>
2025-06-25 15:44:40 -0300

    feat: criando tela de feedback.

---
+++ ecorecicla-app/src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,51 @@
+import { DatePipe, NgForOf, NgIf } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { provideNativeDateAdapter } from "@angular/material/core";
+import { DATE_CONFIG_PROVIDERS } from '../../core/date-format.config';
+
+@Component ({
+    selector: 'app-pages-coleta-cadastro',
+    templateUrl: './feedback.component.html',
+    imports: [
+        NgForOf,
+        NgIf,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        RouterLink,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatDatepickerModule
+    ],
+    providers: [provideNativeDateAdapter(), DatePipe, ...DATE_CONFIG_PROVIDERS],
+})
+
+export class PagesFeedbackComponent implements OnInit{
+
+    public form = new FormGroup({
+        clienteId: new FormControl('', [Validators.required]),
+        dataColeta: new FormControl('', [Validators.required]),
+        quantidade: new FormControl('', [Validators.required])
+    });
+
+    constructor(
+        
+    ) {}
+
+    ngOnInit(): void {
+        
+    }
+}
